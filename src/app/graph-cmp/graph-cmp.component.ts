753e4aaec1627d034153c66f2b475e8f28e6b03c
JavierO <javier.ortiz13@upr.edu>
2018-05-12 02:59:49 -0400

    added graphs

--- src/app/graph-cmp/graph-cmp.component.ts
+++ src/app/graph-cmp/graph-cmp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-graph-cmp',
@@ -10,6 +11,51 @@ export class GraphCmpComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
 
+
+    let chart = [];
+
+
+    //to be replaced when we have real data
+    let tags = [];
+    let x = [];
+    let y = [];
+    for(var i=0; i<10; i++){
+      tags.push(i);
+      x.push(i*10);
+      y.push(i*5+2);
+    }
+
+    chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: tags,
+        datasets: [
+          {
+            data: x,
+            borderColor: "#3cba9f",
+            fill: false
+          },
+          {
+            data: y,
+            borderColor: "#ffcc00",
+            fill: false
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
+  }
 }
