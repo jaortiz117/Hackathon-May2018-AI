d83077bbec0e3d5c1deedc08e0788d8cc293874f
JavierO <javier.ortiz13@upr.edu>
2018-05-12 02:26:18 -0400

    menu bar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatMenuModule, MatIconModule, MatGridListModule } from '@angular/material';
+import { MatButtonModule,
+  MatMenuModule,
+  MatIconModule,
+  MatGridListModule,
+  MatDividerModule,
+  MatCardModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule,
     MatMenuModule,
     MatIconModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDividerModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   exports: [MatButtonModule,
     MatMenuModule,
     MatIconModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDividerModule,
+    MatCardModule,
+    MatToolbarModule
   ],
 })
 export class MaterialModule { }
