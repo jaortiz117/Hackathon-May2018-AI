167612249991e91c8041bc689168814661aaa521
JavierO <javier.ortiz13@upr.edu>
2018-05-12 01:49:13 -0400

    ui layout done

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatMenuModule, MatIconModule, MatGridListModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatGridListModule
+  ],
+  exports: [MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatGridListModule
+  ],
 })
 export class MaterialModule { }
